4cdc99e00bac89512bbed438bf1ac0376546c164
Ben <ben.murphy@live.ie>
2025-01-17 14:47:37 +1100

    logic for removing classic scoring leagues

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from app import app
 from app.forms import LeagueIDForm
 import json
@@ -23,7 +23,7 @@ def user_input_league_id():
     form = LeagueIDForm()
 
     if form.validate_on_submit():
-        return redirect(url_for('success'))
+        return redirect(url_for('chart'))
     
     return render_template('user_input_league_id.html', title='League ID Input', form=form)
 
@@ -40,33 +40,39 @@ def chart():
     response = urlopen(url) 
     
     # storing the JSON response  
-    data_json = json.loads(response.read()) 
+    data_json = json.loads(response.read())
 
-    ids = [i['id'] for i in data_json['league_entries']]
-    names = [i['short_name'] for i in data_json['league_entries']]
-    id_name_map = {i:v for i,v in zip(ids,names)}
+    # if scoring is head-to-head format
+    if data_json['league']['scoring'] == 'h':
 
-    # create dataframe of current standings
-    s_df = pd.DataFrame(data_json['standings'])
-    print(s_df)
-    s_df['player'] = s_df['league_entry'].apply(lambda x: id_name_map[x])
-    player_initials = s_df['player'].values.tolist()
+        ids = [i['id'] for i in data_json['league_entries']]
+        names = [i['short_name'] for i in data_json['league_entries']]
+        id_name_map = {i:v for i,v in zip(ids,names)}
 
-    s_df['average_points_for'] = s_df['points_for'] / (s_df['matches_won'] + s_df['matches_lost'] + s_df['matches_drawn'])
-    s_df['average_points_against'] = s_df['points_against'] / (s_df['matches_won'] + s_df['matches_lost'] + s_df['matches_drawn'])
+        # create dataframe of current standings
+        s_df = pd.DataFrame(data_json['standings'])
+        print(s_df)
+        s_df['player'] = s_df['league_entry'].apply(lambda x: id_name_map[x])
+        player_initials = s_df['player'].values.tolist()
 
-    x = s_df['average_points_for'].values.tolist()
-    y = s_df['average_points_against'].values.tolist()
+        s_df['average_points_for'] = s_df['points_for'] / (s_df['matches_won'] + s_df['matches_lost'] + s_df['matches_drawn'])
+        s_df['average_points_against'] = s_df['points_against'] / (s_df['matches_won'] + s_df['matches_lost'] + s_df['matches_drawn'])
 
-    del(s_df)
+        x = s_df['average_points_for'].values.tolist()
+        y = s_df['average_points_against'].values.tolist()
 
-    data_dict = []
-    for i,v in zip(x, y):
-        d = {'x': i, 'y': v}
-        data_dict.append(d)
- 
-    return render_template(
-        template_name_or_list='chart.html',
-        league_id=league_id,
-        data_dict=data_dict,
-        labels=player_initials)
+        del(s_df)
+
+        data_dict = []
+        for i,v in zip(x, y):
+            d = {'x': i, 'y': v}
+            data_dict.append(d)
+    
+        return render_template(
+            template_name_or_list='chart.html',
+            league_id=league_id,
+            data_dict=data_dict,
+            labels=player_initials)
+    else:
+        flash('Only head-to-head leagues are currently supported. Try again with a different League ID.')
+        return redirect(url_for('user_input_league_id'))
