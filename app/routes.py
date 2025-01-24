a0c3c954616755dbc9f67310cb520ce556c4e380
Ben <ben.murphy@live.ie>
2025-01-24 15:15:12 +1100

    style changes

--- app/routes.py
+++ app/routes.py
@@ -41,6 +41,8 @@ def chart():
     # if scoring is head-to-head format
     if data_json['league']['scoring'] == 'h':
 
+        league_details = data_json['league']
+        league_name = league_details['name']
 
         # scatter plot
         ids = [i['id'] for i in data_json['league_entries']]
@@ -49,27 +51,22 @@ def chart():
 
         # create dataframe of current standings
         s_df = pd.DataFrame(data_json['standings'])
-        print(s_df)
+
         s_df['player'] = s_df['league_entry'].apply(lambda x: id_name_map[x])
         player_initials = s_df['player'].values.tolist()
 
         s_df['average_points_for'] = s_df['points_for'] / (s_df['matches_won'] + s_df['matches_lost'] + s_df['matches_drawn'])
         s_df['average_points_against'] = s_df['points_against'] / (s_df['matches_won'] + s_df['matches_lost'] + s_df['matches_drawn'])
 
-        x = s_df['average_points_for'].values.tolist()
-        y = s_df['average_points_against'].values.tolist()
+        pts_for = s_df['average_points_for'].values.tolist()
+        pts_against = s_df['average_points_against'].values.tolist()
 
-        del(s_df)
-
-        data_dict = []
-        for i,v in zip(x, y):
+        scatter_data = []
+        for i,v in zip(pts_for, pts_against):
             d = {'x': i, 'y': v}
-            data_dict.append(d)
+            scatter_data.append(d)
 
         # expected league table
-
-
-
         matches = pd.DataFrame(data_json['matches'])
 
         matches1 = matches.loc[matches['finished']==True]
@@ -141,12 +138,14 @@ def chart():
         return render_template(
             template_name_or_list='chart.html',
             league_id=league_id,
-            data_dict=data_dict,
+            data_dict=scatter_data,
             labels=player_initials,
 
             column_names=xlt_col_names, 
             row_data=xlt_row_data,
-            zip=zip)
+            zip=zip,
+            
+            league_name=league_name)
     
     else:
         flash('Only head-to-head leagues are currently supported. Try again with a different League ID.')
